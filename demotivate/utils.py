c4bd110a46218e1dc7f426db894879b64face600
SolovovChann <solnik2001@gmail.com>
2022-05-11 00:39:14 +0500

    feature: Placeholder util class

--- demotivate/utils.py
+++ demotivate/utils.py
@@ -1,6 +1,6 @@
 from typing_extensions import TypeAlias
 from io import BytesIO
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 
 Color: TypeAlias = int | str | tuple[int, int, int] | tuple[int, int, int, int]
@@ -32,3 +32,37 @@ def format_padding(padding: int | Indent) -> Indent:
         case object:
             message = 'Padding type "%s" is not supported' % type(object)
             raise ValueError(message)
+
+
+class Placeholder:
+    """
+    # Placeholder
+    Create test image
+    """
+
+    def __init__(
+        self,
+        font: str,
+        background: str = '#5576d1',
+        foreground: str = '#fff'
+    ):
+        self.font = font
+        self.background = background
+        self.foreground = foreground
+
+    def __call__(self, width: int, height: int) -> Image.Image:
+        """
+        ## Placeholder
+        Create image with set width and height
+        """
+        size = width, height
+        text = '%ix%i' % size
+        xy = tuple(i // 2 for i in size)
+
+        image = Image.new('RGB', size, self.background)
+        font = ImageFont.truetype(self.font, min(size) // 6)
+        overlay = ImageDraw.Draw(image, 'RGB')
+
+        overlay.text(xy, text, self.foreground, font, 'mm', align='center')
+
+        return image
