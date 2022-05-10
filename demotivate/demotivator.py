aa6353755ec9172cb0f4e00ec63d730e7aceb0c3
SolovovChann <solnik2001@gmail.com>
2022-05-11 00:32:50 +0500

    feature: draw inner frame method

--- demotivate/demotivator.py
+++ demotivate/demotivator.py
@@ -21,3 +21,22 @@ class Demotivator:
         def __init__(self, padding: Indent, margin: Indent) -> None:
             self.padding = format_padding(padding)
             self.margin = format_padding(margin)
+
+        def draw_inner_frame(self, source: Image.Image) -> Image.Image:
+            """
+            ## Frame.draw_inner_frame()
+            Draw a frame from image to frame
+            """
+            expand = (
+                source.width + sum(self.padding[::2]),
+                source.height + sum(self.padding[1::2]))
+            inner = Image.new('RGB', expand, self.background)
+            inner.paste(source, self.padding[:2])
+
+            draw = ImageDraw.Draw(inner, 'RGB')
+            draw.rectangle(
+                (0, 0) + expand,
+                outline=self.foreground,
+                width=self.width)
+
+            return inner
