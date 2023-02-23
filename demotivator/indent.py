6bef486b8faca937f357bf5a4cd0e0482f2c3d68
SolovovChann <solnik2001@gmail.com>
2023-02-23 20:33:39 +0500

    feature: ImageIndentation.css_like class method

--- demotivator/indent.py
+++ demotivator/indent.py
@@ -12,6 +12,25 @@ class ImageIndentation:
     right: int
     bottom: int
 
+    @classmethod
+    def css_like(cls, *indent: int) -> 'ImageIndentation':
+        match indent:
+            case (int(), ) as padding:
+                padding = padding[0]
+                return cls(padding, padding, padding, padding)
+            case vertical, horizontal:
+                return cls(horizontal, vertical, horizontal, vertical)
+            case top, horizontal, bottom:
+                return cls(horizontal, top, horizontal, bottom)
+            case left, top, right, bottom:
+                return cls(left, top, right, bottom)
+            case [*padding] if len(padding) > 4:
+                message = 'Indentation with %i elements is not supported'
+                raise TypeError(message % len(padding))
+            case _:
+                message = 'Undefined indentation type: %s'
+                raise TypeError(message % type(indent))
+
     def expand(self, image: Image.Image, color: Color = '#000') -> Image.Image:
         width = image.width + self.left + self.right
         height = image.height + self.top + self.bottom
