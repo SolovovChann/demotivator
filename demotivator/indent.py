2fb62218b9d80ea48525edc25e89e252e0f0456a
SolovovChann <solnik2001@gmail.com>
2023-02-23 20:33:04 +0500

    feature: ImageIndentation.expand method

--- demotivator/indent.py
+++ demotivator/indent.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
 
+from PIL import Image
+
+from demotivator.typing import Color
+
 
 @dataclass
 class ImageIndentation:
@@ -7,3 +11,12 @@ class ImageIndentation:
     top: int
     right: int
     bottom: int
+
+    def expand(self, image: Image.Image, color: Color = '#000') -> Image.Image:
+        width = image.width + self.left + self.right
+        height = image.height + self.top + self.bottom
+
+        extended = Image.new(image.mode, (width, height), color)
+        extended.paste(image, (self.left, self.top))
+
+        return extended
