2ccf79740eacb285a5f1d4727d9b1df5713da88c
SolovovChann <solnik2001@gmail.com>
2023-02-23 20:15:57 +0500

    test: add _images_are_similar util function

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,5 +1,12 @@
 from unittest import TestCase
 
+from PIL import Image, ImageChops
+
+
+def _images_are_similar(first: Image.Image, second: Image.Image) -> bool:
+    difference = ImageChops.difference(first, second)
+    return difference.getbbox() is None
+
 
 class TestLoadImage(TestCase):
     """Test private `_load_image` function"""
