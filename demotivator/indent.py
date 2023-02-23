c7f7a1e83f12a50330bb4c4e3aaa6b4b4c3d6e2a
SolovovChann <solnik2001@gmail.com>
2023-02-23 20:34:08 +0500

    docs: add ImageIndentation.css_like doc string

--- demotivator/indent.py
+++ demotivator/indent.py
@@ -14,6 +14,21 @@ class ImageIndentation:
 
     @classmethod
     def css_like(cls, *indent: int) -> 'ImageIndentation':
+        """
+        Create indentation with 1-4 values:
+
+        1. Indentations will be set simultaneously from each edge.
+
+        2. The first value sets the indentations from the
+        top and bottom edges, the second - from the left and right.
+
+        3. The first value sets the margin from the top edge,
+        the second value from both the left and right edges, and the
+        third value from the bottom edge.
+
+        4. Alternately sets the indentations from the
+        top, right, bottom, and left edges.
+        """
         match indent:
             case (int(), ) as padding:
                 padding = padding[0]
