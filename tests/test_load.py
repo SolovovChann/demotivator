28c2f11b86ae0418af9dd41155764a5df956fde2
SolovovChann <solnik2001@gmail.com>
2023-02-23 20:20:02 +0500

    test: _load_image str test

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,6 +1,11 @@
+from pathlib import Path
 from unittest import TestCase
 
 from PIL import Image, ImageChops
+
+from demotivator import _load_image
+
+base_dir = Path(__file__).parent.parent
 
 
 def _images_are_similar(first: Image.Image, second: Image.Image) -> bool:
@@ -10,6 +15,15 @@ def _images_are_similar(first: Image.Image, second: Image.Image) -> bool:
 
 class TestLoadImage(TestCase):
     """Test private `_load_image` function"""
+
+    path: Path = Path(base_dir) / 'capybara.jpg'
+    string: str = str(path)
+    image: Image.Image = Image.open(path).convert('RGB')
+
+    def test_str(self) -> None:
+        image = _load_image(self.string).convert('RGB')
+        self.assertIsInstance(image, Image.Image)
+        self.assertTrue(_images_are_similar(image, self.image))
 
 
 class TestLoadFont(TestCase):
