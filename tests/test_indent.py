fe62261d48dc8520a245a7e0d83c033b6b4e4229
SolovovChann <solnik2001@gmail.com>
2023-02-23 20:06:57 +0500

    test: IndentationIndent.indent size test

--- tests/test_indent.py
+++ tests/test_indent.py
@@ -1,8 +1,23 @@
 from unittest import TestCase
 
+from PIL import Image
+
+from demotivator.indent import ImageIndentation
+
 
 class TestIndentationIndent(TestCase):
     """Test `Indentation.indent()` method"""
+
+    image = Image.new('RGB', (255, 127), 'magenta')
+
+    def test_indent_size(self) -> None:
+        indent = ImageIndentation.css_like(10)
+        indented = indent.expand(self.image, '#fff')
+        width = self.image.width + 10 + 10
+        height = self.image.height + 10 + 10
+
+        self.assertEqual(indented.width, width)
+        self.assertEqual(indented.height, height)
 
 
 class TestIndentionCssLike(TestCase):
