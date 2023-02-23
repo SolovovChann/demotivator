e966328af623d6c12d81a372598298270265856a
SolovovChann <solnik2001@gmail.com>
2023-02-23 20:05:21 +0500

    test: add Indentation.css_like test case

--- tests/test_indent.py
+++ tests/test_indent.py
@@ -5,5 +5,5 @@ class TestIndentationIndent(TestCase):
     """Test `Indentation.indent()` method"""
 
 
-
-
+class TestIndentionCssLike(TestCase):
+    """Test `Indentation.css_like()` class method"""
