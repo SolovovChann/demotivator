863e7f1b5aec5fa7987567117dfabfe34159056e
SolovovChann <solnik2001@gmail.com>
2023-02-23 20:13:36 +0500

    test: add TestLoadFont test case

--- tests/test_load.py
+++ tests/test_load.py
@@ -3,3 +3,7 @@ from unittest import TestCase
 
 class TestLoadImage(TestCase):
     """Test private `_load_image` function"""
+
+
+class TestLoadFont(TestCase):
+    """Test private `_load_font` function"""
